a3a2b078f765a7003dc6308079bdbab06ed4dbda
Kim Astrom <kim.astrom@gmail.com>
2012-01-09 10:57:35 +0100

    search light version

--- web/controller/SearchController.php
+++ web/controller/SearchController.php
@@ -18,6 +18,7 @@ class SearchController
         $this->_langHandler = new LanguageHandler($this->_dbHandler);
         $this->_searchView = new SearchView();
         $this->_snippetView = new SnippetView();
+        $this->_pagingHandler = new PagingHandler($this->_snippetHandler->getAllSnippets(), 1, 3);
         $this->_html = '';
     }
 
@@ -28,16 +29,20 @@ class SearchController
         if($this->_searchView->doSearch()) {
             $searchQuery = $this->_searchView->getSearchQuery();
             $lang = $this->_searchView->getSearchLang();
+            //För pageningen
+            $arrToPreventError = array(1,1,1,1,1);
             /**
              * OBS! Följande kod är bortkommenterat eftersom jag inte vet vilka kriterier man vill söka efter
              * just nu kan man skriva ord från title, code eller desc samt välja språk (PHP som första val)
              */
             //$this->_html .= $this->_searchView->searchAnswerView($this->_snippetHandler->searchByTitle($searchQuery));
-            $this->_html .= $this->_snippetView->listView($this->_snippetHandler->searchByTitle($searchQuery));
+            $this->_html .= $this->_snippetView->listView($this->_snippetHandler->searchByTitle($searchQuery),1,$arrToPreventError,1);
             //$this->_html .= $this->_searchView->searchAnswerView($this->_snippetHandler->searchByDescription($searchQuery));
             //$this->_html .= $this->_searchView->searchAnswerView($this->_snippetHandler->searchByCode($searchQuery));
             //$this->_html .= $this->_searchView->searchAnswerView($this->_snippetHandler->fullSearch($searchQuery));
-            //$this->_html .= $this->_searchView->searchAnswerView($this->_snippetHandler->fullSearchWithLang($searchQuery, $lang));
+            //$this->_html .= $this->_searchView->searchAnswerView($this->_snippetHandler->fullSearchWithLang($searchQuery, $lang));            
+            
+            
         }
         
         return $this->_html;
