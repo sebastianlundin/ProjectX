1c1cf213c65e7fa4b5cd667777ffde3902dfaa28
Marta Johnsson <marta@longislanddivers.com>
2012-02-16 02:23:12 +0100

    la till mail mapen

---
+++ web/mail/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+$mail = $_POST['mail'];
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$text = $_POST['text'];
+	
+ //$to = "marta@longislanddivers.com";
+ $to = $mail;
+ $message =" You received  a mail from ".$mail;
+ $message .=" Text of the message : ".$text;
+
+ if(mail($to, $subject,$message)){
+	echo "mail successful send";
+} 
+else{ 
+	echo "there's some errors to send the mail, verify your server options";
+}
