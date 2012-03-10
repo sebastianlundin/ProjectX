3c64f0f9ce763cb73c573c9a39a3f9a0040305c4
lizanne <lizanne@server.local>
2012-03-10 16:04:57 +0100

    working with comments

--- apps/Source_and_executables/Win_Mac_Linux/cachefuncs.h
+++ apps/Source_and_executables/Win_Mac_Linux/cachefuncs.h
@@ -1,21 +1,27 @@
+// Class (header-file) for loading json-data from a selected cache-file
+
+// Beginning of header-file
 #ifndef CACHEFUNCS_H
 #define CACHEFUNCS_H
 
+// Includes all the importen libs (in qt)
+// that we need to use for this class to work
 #include <QObject>
 #include <QByteArray>
 #include <QString>
 
-class CacheFuncs : public QObject
+// Beginning of the class
+class CacheFuncs : public QObject // Inheritance of the most used class (QObject)
 {
-    Q_OBJECT
+    Q_OBJECT // A macro for making magic happen, when it comes to calling other classes from a class
+
+// Methods for the class
 public:
-    explicit CacheFuncs(QObject *parent = 0);
-    QByteArray GetCacheFileData(QString a_filename);
-    
-signals:
-    
-public slots:
+    explicit CacheFuncs(QObject *parent = 0); // Constructor
+    QByteArray GetCacheFileData(QString a_filename); // Load json-data from a selected cache-file in (~/snippt_cache_files/)
     
 };
+// Ending of the class
 
 #endif // CACHEFUNCS_H
+// Ending of header-file
