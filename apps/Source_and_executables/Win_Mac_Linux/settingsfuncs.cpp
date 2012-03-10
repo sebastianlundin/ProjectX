bc17262dbdf017c0e6e549c2cb6366696a306dd1
lizanne <lizanne@server.local>
2012-03-10 23:14:23 +0100

    working with comments

--- apps/Source_and_executables/Win_Mac_Linux/settingsfuncs.cpp
+++ apps/Source_and_executables/Win_Mac_Linux/settingsfuncs.cpp
@@ -1,8 +1,16 @@
+// Class (header-file) for getting the api-adress from settings
+
+// Includes all the importen libs (in qt)
+// that we need to use for this class to work
 #include "settingsfuncs.h"
 #include <QString>
 #include <QSettings>
 #include <QVariant>
 
+// Get the api-adress from settings.
+// If there isn't any adress in the
+// settings. Use a default-adress of
+// http://tmpn.se/api instead
 QString SettingsFuncs::GetApiUrl()
 {    
     QVariant apiUrl;
