1e2e485b70f6b05350e175d67248db1adc15ccea
lizanne <lizanne@server.local>
2012-03-10 19:36:18 +0100

    working with comments

--- apps/Source_and_executables/Win_Mac_Linux/customcombobox.cpp
+++ apps/Source_and_executables/Win_Mac_Linux/customcombobox.cpp
@@ -1,9 +1,15 @@
+// Subclass for handling some mouse events for the list of previous saved searches
+
+// Includes all the importen libs (in qt)
+// that we need to use for this class to work
 #include "customcombobox.h"
 #include <QMessageBox>
 #include "mainwindow.h"
 
+// Constructor
 CustomComboBox::CustomComboBox(QWidget *a_parent) {}
 
+// Handling mouse-events for the list with previous saved searches, so the user can show the list with a click
 void CustomComboBox::mouseReleaseEvent(QMouseEvent *a_event)
 {
     this->showPopup();
