7e856faff176e5ba81292e41bae31f40c6c742d5
lizanne <lizanne@server.local>
2012-03-10 18:47:27 +0100

    working with comments

--- apps/Source_and_executables/Win_Mac_Linux/mainwindow.h
+++ apps/Source_and_executables/Win_Mac_Linux/mainwindow.h
@@ -1,6 +1,11 @@
+// Class (header-file) for working with the main user interface (where you can search snippets etc)
+
+// Beginning of header-file
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+// Includes all the importen libs (in qt)
+// that we need to use for this class to work
 #include <QTreeWidgetItem>
 #include <QVariantList>
 #include <QMainWindow>
@@ -16,14 +21,17 @@
 #include <QTimer>
 #include <libs/code/libqxt/qxtglobalshortcut.h>
 
+// Namespace for the class
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+// Beginning of the class
+class MainWindow : public QMainWindow // Subclass of the qwindow, so that we can add extra functionality to the element
 {
-    Q_OBJECT
+    Q_OBJECT // A macro for making magic happen, when it comes to calling other classes from a class
 
+// Member variables of the class
 private:
     Ui::MainWindow *ui;
     ApiFuncs *apiFuncs;
@@ -38,31 +46,78 @@ private:
     SettingsDialog *settingsDialog;
     QTreeWidgetItem *group;
 
+// Methods for the class
 public:
+
+    // Move the window to the center of the screen
     void CenterWindow();
+
+    // Constructor
     explicit MainWindow(QWidget *parent = 0);
-    void RenderClass();
+
+    // Test the connection to the api (thru another method in another class), and show a message
     void ShowPossiblyErrorAboutConnection();
+
+    // Fill the tree with snippets
     void FillListWithSnippets(QVariantList a_jsonObject);
+
+    // List files of cached searches
     void ListSearchFiles();
+
+    // Destruktor
     ~MainWindow();
 
+// Slots for the class (a slot is a method, that a signal can call (like events))
 private slots:
+
+    // Open the about box
     void on_aboutSnippt_triggered();
+
+    // Load the global keyboard shortcuts from settings
     void KeyboardActions();
+
+    // Raise the window (if its minimized) and clear searchfields etc
     void ShowWindowAndFocusSearchField();
+
+    // Show all elements in the window
     void ShowAllElements();
+
+    // Show just a few elements (like searchfield, searchbutton, previous searches and deletebutton) in the window
     void ShowAndHideElementsWithNewSearch();
+
+    // Listen to events in the window. This one listen to when the window minimizes
+    // and hide all elements in window, except: searchfield, searchbutton, previous searches and deletebutton
     bool eventFilter(QObject *a_object, QEvent *a_event);
+
+    // Search for a snippet, update fields, listing the snippets, show alerts and so on
     void SearchSnippet();
+
+    // Update the search animation with dots and stop the timer if the api
+    // find something to put inside the tree with snippets
     void UpdateSearchAnimation();
+
+    // Show the selected snippet
     void ShowSelectedSnippet(QTreeWidgetItem *a_item, int a_column);
+
+    // Clear all fields for the searchstring, selected snippet, listed snippets and so on
     void ClearFields();
+
+    // Fill the list with previous searches with cached searches
     void FillListWithPrevSearches(int a_index);
+
+    // Copy the selected snippet to clipboard
     void CopySelectedSnippet();
+
+    // Button for copy the selected snippet to clipboard
     void on_copySnippet_clicked();
+
+    // Open preferences dialog
     void on_actionPreferences_triggered();
+
+    // Button for searching a snippet from a given searchstring
     void on_searchSnippet_clicked();
+
+    // Delete a selected previous search
     void on_deleteSelectedPrevSearch_clicked();
 };
 
