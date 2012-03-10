6c3b79f28d37d1e2af820d194efb79b95f5c9522
lizanne <lizanne@server.local>
2012-03-10 15:58:24 +0100

    working with comments

--- apps/Source_and_executables/Win_Mac_Linux/apifuncs.h
+++ apps/Source_and_executables/Win_Mac_Linux/apifuncs.h
@@ -1,6 +1,11 @@
+// Class (header-file) for handling connection to the API and saving JSON-data to cache-files
+
+// Beginning of header-file
 #ifndef APIFUNCS_H
 #define APIFUNCS_H
 
+// Includes all the importen libs (in qt)
+// that we need to use for this class to work
 #include "filefuncs.h"
 #include <QEvent>
 #include <QObject>
@@ -9,18 +14,23 @@
 #include <QByteArray>
 #include "jsonfuncs.h"
 
-class ApiFuncs : public QObject
+// Beginning of the class
+class ApiFuncs : public QObject // Inheritance of the most used class (QObject)
 {
-    Q_OBJECT
+    Q_OBJECT // A macro for making magic happen, when it comes to calling other classes from a class
 
+// Member variables of the class
 private:
     FileFuncs *fileFuncs;
     JsonFuncs *jsonFuncs;
 
+// Methods for the class
 public:
-    explicit ApiFuncs(QObject *parent = 0);
-    void ConnectToApi(QString a_filename, QString a_url, bool &a_errorTest, QString a_search);
-    void LoadApiData(QString a_filename, QByteArray a_data, QString a_search);
+    explicit ApiFuncs(QObject *parent = 0); // Constructor
+    void ConnectToApi(QString a_filename, QString a_url, bool &a_errorTest, QString a_search); // Connect to API, and test if the URL is working
+    void SaveApiData(QString a_filename, QByteArray a_data, QString a_search); // Save loaded json-data (in the above method) to a cache-file in (~/snippt_cache_files/)
 };
+// Ending of the class
 
 #endif // APIFUNCS_H
+// Ending of header-file
