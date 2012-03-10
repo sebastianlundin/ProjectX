065f498842ab2225ef18fdd7250b4e1f41991def
lizanne <lizanne@server.local>
2012-03-10 18:21:30 +0100

    working with comments

--- apps/Source_and_executables/Win_Mac_Linux/jsonfuncs.h
+++ apps/Source_and_executables/Win_Mac_Linux/jsonfuncs.h
@@ -1,19 +1,29 @@
-// Header-file for the class with the responsibility
-// of parsing JSON-data and returning an object
+// Class (header-file) for parsing json-data
 
+// Beginning of header-file
 #ifndef JSONFUNCS_H
 #define JSONFUNCS_H
 
+// Includes all the importen libs (in qt)
+// that we need to use for this class to work
 #include <QVariantList>
 #include <QByteArray>
 
-// The class for the above description
+// Beginning of the class
 class JsonFuncs
 {
+
+// Methods for the class
 public:
-    // The method that make the magic happen
+
+    // Parse given data with the json-parser, and return
+    // an object with parsed json-data
     QVariantList GetJsonObject(QByteArray a_jsonData);
+
+    // Get json-data with code for a specific snippet
     QString GetSnippetCode(QVariantList a_jsonObject);
 };
+// Ending of the class
 
 #endif // JSONFUNCS_H
+// Ending of header-file
